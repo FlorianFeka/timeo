6cb8428eff17c5fa652e7c9900b9c589bbd6f4ae
FlorianFeka <florianfeka05@gmail.com>
2020-07-27 23:02:21 +0200

    add material buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { TimerComponent } from './timer/timer.component';
@@ -12,6 +14,8 @@ import { TimerComponent } from './timer/timer.component';
   declarations: [AppComponent, TimerComponent],
   imports: [
     MatSliderModule,
+    MatIconModule,
+    MatButtonModule,
     BrowserModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -7,10 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
   @Input() private time: number;
-  seconds: number;
-  minutes: number;
-  hours: number;
   parsedTime: Date;
+  intervalId: number;
   output: string;
 
   constructor() {}
@@ -18,33 +16,20 @@ export class TimerComponent implements OnInit {
   ngOnInit(): void {
     this.parsedTime = new Date(null);
     this.parsedTime.setSeconds(this.time);
-    let id = setInterval(() => {
+    this.startInterval();
+  }
+
+  startInterval() {
+    this.intervalId = setInterval(() => {
       this.time -= 1;
       this.output = `${new Date(this.time * 1000).toISOString().substr(11, 8)}`;
       if (this.time == 0) {
-        clearInterval(id);
+        this.stopInterval();
       }
     }, 1000);
   }
 
-  decreaseTimer() {
-    if (this.seconds > 0) {
-      this.seconds -= 1;
-      return;
-    } else {
-      this.seconds = 59;
-      this.minutes -= 1;
-    }
-    if (this.minutes > 0) {
-      this.minutes -= 1;
-      return;
-    } else {
-      if (this.hours > 0) {
-        this.seconds = 59;
-        this.minutes = 59;
-        this.hours -= 1;
-      }
-      return;
-    }
+  stopInterval() {
+    clearInterval(this.intervalId);
   }
 }
