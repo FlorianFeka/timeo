8d94ba8901588086b518e1be19df9b63b4c2940e
Florian Feka <florianfeka05@gmail.com>
2021-03-29 18:07:30 +0000

    update rxjs

--- src/app/timer-creator/timer-creator.component.spec.ts
+++ src/app/timer-creator/timer-creator.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TimerCreatorComponent } from './timer-creator.component';
 
@@ -6,7 +6,7 @@ describe('TimerCreatorComponent', () => {
   let component: TimerCreatorComponent;
   let fixture: ComponentFixture<TimerCreatorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TimerCreatorComponent ]
     })
